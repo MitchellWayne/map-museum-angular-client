1728b7601f854914cec60804396da9a0db5eeb2e
MWayneMercado <MWayneMercado@gmail.com>
2022-02-15 12:16:12 -0800

    generate component header-logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MapComponent } from './components/map/map.component';
 import { HeaderSearchbarComponent } from './components/header/header-searchbar/header-searchbar.component';
+import { HeaderLogoComponent } from './components/header/header-logo/header-logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    MapComponent,
     HeaderSearchbarComponent,
+    HeaderLogoComponent
   ],
   imports: [
     BrowserModule
