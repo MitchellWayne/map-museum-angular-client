3b0e8f18ca8aff546ab5d61dcc10b42f8ee72d03
MWayneMercado <MWayneMercado@gmail.com>
2022-02-15 13:34:50 -0800

    add fa search icon

--- src/app/components/header/header-searchbar/header-searchbar.component.ts
+++ src/app/components/header/header-searchbar/header-searchbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header-searchbar',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderSearchbarComponent implements OnInit {
   searchQuery!: string;
+  faSearchIcon = faSearch;
 
   constructor() { }
 
