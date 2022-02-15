db35ff99ce6e830658e1ad8c9650c11413eccdc0
MWayneMercado <MWayneMercado@gmail.com>
2022-02-15 12:16:03 -0800

    generate component header-searchbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MapComponent } from './components/map/map.component';
+import { HeaderSearchbarComponent } from './components/header/header-searchbar/header-searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MapComponent
+    HeaderSearchbarComponent,
   ],
   imports: [
     BrowserModule
