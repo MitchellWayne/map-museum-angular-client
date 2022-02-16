e0777008fa36ffd8643d7323990a582298931673
MWayneMercado <MWayneMercado@gmail.com>
2022-02-16 13:41:35 -0800

    define searchActive and searchQuery subscriptions

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  searchActive!: boolean;
+  searchActiveSubscription!: Subscription;
 
-  constructor() { }
+  searchQuery!: string;
+  searchQuerySubscription!: Subscription;
+
+  constructor(private uiService: UiService) { 
+    this.searchActiveSubscription = this.uiService.onSearchActive().subscribe(
+      value => { 
+        this.searchActive = value;
+      }
+    );
+
+    // This might not necessarily be needed here, just in the dropdown component
+    this.searchQuerySubscription = this.uiService.onUpdateSearchQuery().subscribe(
+      value => {
+        this.searchQuery = value;
+      }
+    );
+  }
 
   ngOnInit(): void {
+  }
+
+  toggleSearchActive() {
+    this.uiService.toggleSearchActive();
+  }
+
+  // This might not necessarily be needed here, just in the searchbar component
+  updateSearchQuery(updatedQuery: string) {
+    this.uiService.updateSearchQuery(updatedQuery);
   }
 
 }
