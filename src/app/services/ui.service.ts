1122402b5fe5435836c589163abdaff0940250fc
MWayneMercado <MWayneMercado@gmail.com>
2022-02-16 12:15:27 -0800

    define Subject for search query in Observable

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
   private searchActive: boolean = false;
+  private searchActiveSubject = new Subject<any>();
+
+  constructor() { }
 
   toggleSearchActive(): void {
     this.searchActive = !this.searchActive;
+    this.searchActiveSubject.next(this.searchActive);
   }
 
-  constructor() { }
+  onToggle(): Observable<any> {
+    return this.searchActiveSubject.asObservable();
+  }
 }
