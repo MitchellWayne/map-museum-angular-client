4bb5186926b998b792c23ae67f2babd6c4df4e6d
MWayneMercado <MWayneMercado@gmail.com>
2022-02-17 17:13:52 -0800

    define interfaces for models from Mongodb

---
+++ src/app/interfaces/Note.ts
@@ -0,0 +1,10 @@
+export interface Note {
+  _id: string;
+  series: string;
+  title: string;
+  location: string;
+  synopsis: string;
+  locdetails: string;
+  latlong: string;
+  image: string;
+}
