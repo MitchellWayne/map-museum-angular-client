9905f3fbd1baa6f896d19fb9b6d78d2ace896fef
MWayneMercado <MWayneMercado@gmail.com>
2022-02-17 17:20:43 -0800

    define getSeriesList

--- src/app/services/series.service.ts
+++ src/app/services/series.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Series } from '../interfaces/Series';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeriesService {
+  private apiUrl = 'http://localhost:5000/series';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSeriesList(query: string): Observable<Series[]> {
+    return this.http.get<Series[]>(`${this.apiUrl}/series/?seriesfilter=${query}`);
+  }
 }
