777055ccce9245ddca188394d8e6ba3c6132fb62
MWayneMercado <MWayneMercado@gmail.com>
2022-02-19 13:39:09 -0800

    load google.maps.Map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  loader = new Loader({
+    apiKey: "AIzaSyDQzXxQOdyekwryvLgymDv4yG-VwssetB0",
+    version: "weekly"
+  });
+
+  // map!: google.maps.Map;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loader.load()
+    .then((google) => {
+      new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+        fullscreenControl: false,
+        mapTypeControl: false,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
 }
