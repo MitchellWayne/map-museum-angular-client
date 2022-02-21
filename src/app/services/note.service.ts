238695daf6c01ae980b5f757a0174c97835dbf46
MWayneMercado <MWayneMercado@gmail.com>
2022-02-21 12:01:41 -0800

    define functions for note service

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Note } from '../interfaces/Note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
+  private apiUrl = 'http://localhost:5000/note';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Get notes (minimal)
+  getNoteList(query: string): Observable<Note[]> {
+    return this.http.get<Note[]>(`${this.apiUrl}/?seriesfilterID=${query}`);
+  }
+
+  // Get notes (detailed)
+  getNoteListDetailed(query: string): Observable<Note[]> {
+    return this.http.get<Note[]>(`${this.apiUrl}/detailed/?seriesfilterID=${query}`);
+  }
+
+  // Get note (by ID)
+  getNoteByID(id: string): Observable<Note> {
+    return this.http.get<Note>(`${this.apiUrl}/${id}`);
+  }
+
 }
