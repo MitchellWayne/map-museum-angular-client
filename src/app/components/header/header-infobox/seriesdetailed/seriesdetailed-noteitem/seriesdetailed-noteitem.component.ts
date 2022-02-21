2cac8f740db56a8196595f7d4eee3c378b60bac7
MWayneMercado <MWayneMercado@gmail.com>
2022-02-21 12:37:05 -0800

    seriesdetailed to display notes from uiservice

--- src/app/components/header/header-infobox/seriesdetailed/seriesdetailed-noteitem/seriesdetailed-noteitem.component.ts
+++ src/app/components/header/header-infobox/seriesdetailed/seriesdetailed-noteitem/seriesdetailed-noteitem.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Note } from 'src/app/interfaces/Note';
 
 @Component({
   selector: 'app-seriesdetailed-noteitem',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seriesdetailed-noteitem.component.scss']
 })
 export class SeriesdetailedNoteitemComponent implements OnInit {
+  @Input() note!: Note;
 
   constructor() { }
 
--- src/app/components/header/header-infobox/seriesdetailed/seriesdetailed.component.ts
+++ src/app/components/header/header-infobox/seriesdetailed/seriesdetailed.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Series } from 'src/app/interfaces/Series';
-import { faQuestion, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { SeriesService } from 'src/app/services/series.service';
-import { UiService } from 'src/app/services/ui.service';
 import { Note } from 'src/app/interfaces/Note';
+
+import { SeriesService } from 'src/app/services/series.service';
+import { NoteService } from 'src/app/services/note.service';
+import { UiService } from 'src/app/services/ui.service';
+
+import { faQuestion, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-seriesdetailed',
@@ -12,18 +16,32 @@ import { Note } from 'src/app/interfaces/Note';
 })
 export class SeriesdetailedComponent implements OnInit {
   @Input() series!: Series;
-  notes: Note[] = [];
   seriesImgSrc: string = "";
+
+  notes: Note[] = [];
+  notesSubscription!: Subscription;
 
   faQuestion = faQuestion;
   faArrowLeft = faArrowLeft;
 
-  constructor( private seriesService: SeriesService, private uiService: UiService ) { }
+  constructor( 
+    private seriesService: SeriesService,
+    private uiService: UiService,
+  ){
+    this.notesSubscription = this.uiService.onNoteListUpdate().subscribe(
+      (value) => {
+        this.notes = value;
+        console.log(value);
+        console.log(this.notes);
+      });
+  }
 
   ngOnInit(): void {
     if (this.series.mainImage) {
       this.seriesImgSrc = this.seriesService.getSeriesMainImage(this.series);
     }
+
+    this.uiService.getNoteList(this.series._id);
   }
 
   exitActiveSeries() {
