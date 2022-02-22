cccd75aa6e9a7c55028f24a8255b2360f297e1ae
MWayneMercado <MWayneMercado@gmail.com>
2022-02-21 17:15:59 -0800

    testing the maps sdk w/ marker and infowindow

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -7,25 +7,48 @@ import { Loader } from '@googlemaps/js-api-loader';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  map!: google.maps.Map;
+  google!: typeof google;
+
   loader = new Loader({
     apiKey: "AIzaSyDQzXxQOdyekwryvLgymDv4yG-VwssetB0",
     version: "weekly"
   });
 
-  // map!: google.maps.Map;
-
   constructor() { }
 
   ngOnInit(): void {
+    // Load map at default coordinates
     this.loader.load()
     .then((google) => {
-      new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      this.google = google;
+      this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 45.9795412, lng: -51.6052898 },
         zoom: 2,
         fullscreenControl: false,
         mapTypeControl: false,
         streetViewControl: false,
       });
+
+      // Infowindow tests
+      let infowindow = new google.maps.InfoWindow();
+      let popuphtml = "<div>TEST POPUP <div>INNER TEST</div></div>";
+      infowindow.setContent(popuphtml);
+
+       // Test markers
+      const marker = new google.maps.Marker({
+        position: {lat: 1, lng: 1},
+        map: this.map,
+        title: "Test marker",
+      });
+
+      marker.addListener('click', () => {
+        console.log(marker);
+        console.log(google);
+        console.log(this.google);
+        infowindow.open(this.map, marker);
+      });
+
     })
     .catch(err => {
       console.log(err);
