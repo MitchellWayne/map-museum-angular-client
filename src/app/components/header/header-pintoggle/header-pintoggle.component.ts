90ee0a20787054dcb2b192f19c64d843e9e2a4c5
MWayneMercado <MWayneMercado@gmail.com>
2022-02-23 12:02:44 -0800

    add checkbox for toggling pinActive state

--- src/app/components/header/header-pintoggle/header-pintoggle.component.ts
+++ src/app/components/header/header-pintoggle/header-pintoggle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header-pintoggle',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-pintoggle.component.scss']
 })
 export class HeaderPintoggleComponent implements OnInit {
+  toggleValue: boolean = true;
 
-  constructor() { }
+  constructor(private uiService : UiService) { }
 
   ngOnInit(): void {
+  }
+
+  toggleChanged(){
+    this.uiService.setPinsActive(this.toggleValue);
   }
 
 }
