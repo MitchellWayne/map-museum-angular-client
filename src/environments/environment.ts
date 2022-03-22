e8e06e4a40bc305dfae7dd10f1d6dc3ed76fd67b
MWayneMercado <MWayneMercado@gmail.com>
2022-03-22 15:45:05 -0700

    rm env map key in favor of api call for key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { MAPS_APIKEY } from './env';
 
 export const environment = {
   production: false,
-  MAPS_APIKEY: MAPS_APIKEY,
 };
 
 /*
