9b0369a8c29c012d400797fd83633d5699fb97e9
MWayneMercado <MWayneMercado@gmail.com>
2022-03-22 15:51:21 -0700

    define map service to get api key

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
+  private apiUrl = 'http://localhost:5000/series'; // to be changed to work w/ heroku
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMapKey(): Observable<string> {
+    return this.http.get<string>(`${this.apiUrl}/admin/mapsAPI`);
+  }
 }
